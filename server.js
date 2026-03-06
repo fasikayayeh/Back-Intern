8f40789bbb55a09a1fe52a53e1825e9d6f362ac9
fasika <fasikayayeh3gmail.com>
2026-03-06 12:31:15 -0800

    Add root route

--- server.js
+++ server.js
@@ -15,6 +15,11 @@ connectDB()
 
 app.use("/api/applications", applicationRoutes)
 
-app.listen(5000, ()=>{
- console.log("Server running on port 5000")
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
